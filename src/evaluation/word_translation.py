44ab92d323cee44792c45671bd808ab025b5808e
maxwell <34574819+Slayt3r@users.noreply.github.com>
2023-01-21 22:47:44 +0200

    #pairs = sorted(pairs, key=lambda x: word2id1[x[0]])

--- src/evaluation/word_translation.py
+++ src/evaluation/word_translation.py
@@ -81,7 +81,7 @@ def load_dictionary(path, word2id1, word2id2):
                    not_found, not_found1, not_found2))
 
     # sort the dictionary by source word frequencies
-    pairs = sorted(pairs, key=lambda x: word2id1[x[0]])
+    #pairs = sorted(pairs, key=lambda x: word2id1[x[0]])
     dico = torch.LongTensor(len(pairs), 2)
     for i, (word1, word2) in enumerate(pairs):
         dico[i, 0] = word2id1[word1]
